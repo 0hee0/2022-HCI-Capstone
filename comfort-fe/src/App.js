0157875aaf6feb4a2af7a39949d2262ce0d0a4c2
0hee0 <suhee0206@gmail.com>
2022-11-10 23:02:52 +0900

    feat: add theme and font

--- comfort-fe/src/App.js
+++ comfort-fe/src/App.js
@@ -1,8 +1,22 @@
+import React, { Suspense } from 'react';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import "./font.module.css";
+import { theme } from './theme.js';
+
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const customTheme = createTheme(theme);
+
+    return (
+        <ThemeProvider theme={customTheme}>
+            <CssBaseline />
+            <Suspense fallback={(<div>Loading...</div>)}>
+                
+            </Suspense>
+        </ThemeProvider>
+    );
 }
 
 export default App;
---
+++ comfort-fe/src/theme.js
@@ -0,0 +1,25 @@
+export const theme = {
+    typography: {
+        fontFamily: [
+            "S-CoreDream",
+        ].join(',')
+    },
+    palette: {
+        primary: {
+            main: "#041562",
+        },
+        secondary: {
+            main: "#F3F8FF"
+        },
+        accent: {
+            main: "rgb(232, 211, 255, 0.5)",
+            dark: "#E8D3FF"
+        },
+        background: {
+            main: "#F3F8FF"
+        },
+        gray: {
+            main: "#DDDDDD"
+        }
+    }
+}
