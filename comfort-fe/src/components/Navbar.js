9f462ebca63637d9da1869ce27035129ee6d50d9
0hee0 <suhee0206@gmail.com>
2022-11-10 23:26:22 +0900

    feat: implement navbar ui

--- comfort-fe/src/components/Navbar.js
+++ comfort-fe/src/components/Navbar.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box, Typography, Stack } from '@mui/material';
+import logo from "../assets/heart-attack.png";
+
 
 function Navbar() {
+    const [username, setUsername] = useState("Hee");    // mockup data
+
     return (
-        <div>
-            
-        </div>
+        <Box px={5} py={2} sx={{ boxShadow: "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px" }}>
+            <Stack direction="row" alignItems="center" justifyContent="space-between">
+                <a href="/" style={{ margin: 0, padding: 0, lineHeight: "11px" }}>
+                    <img src={logo} width="50" />
+                </a>
+                <Box backgroundColor="primary.main" py={0.75} px={2.5} borderRadius={"12px"}>
+                    <Typography color="white" fontFamily="Roboto Mono">{username}</Typography>
+                </Box>
+            </Stack>
+        </Box>
     )
 }
 
