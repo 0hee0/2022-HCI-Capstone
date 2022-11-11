b6440efc9738046b12b54fe9b2bf861e85bee958
0hee0 <suhee0206@gmail.com>
2022-11-11 20:08:39 +0900

    feat: implement checklist ui

--- comfort-fe/src/components/Checklist.js
+++ comfort-fe/src/components/Checklist.js
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { dateToString, getDays } from '../utils/format';
+import { Box, Typography, Stack, Button } from '@mui/material';
+import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
+
 
 function Checklist() {
+    const today = new Date();
+    const days = getDays(today);
+    const [flags, setFlags] = useState([true, false, true, false, false, false, false]);  // mockup data
+
+    useEffect(() => {
+        // TODO: API - GET flags
+        // for day in days: 
+        // send day.date and get flag
+        // push flag to flags
+    }, [])
+
+    // const handleClick = (date) => (event) => {
+    //     // TODO: API - POST day.date
+    // }
+
+    /* TEST */
+    const handleClick = (index) => (event) => {
+        let newFlags = [...flags];
+        newFlags[index] = !flags[index];
+        setFlags(newFlags);
+    }
+
     return (
-        <div>
+        <Stack py={3} spacing={1} justifyContent="center" alignItems="center">
+            <Typography fontWeight="700">{dateToString(today)} (오늘)</Typography>
+            <hr style={{ width: "100%", border: "1px solid #dddddd", margin: "1rem 0" }} />
             
-        </div>
+            <Stack direction="row" spacing={4.5}>
+                {days.map((day, index) =>
+                    <Stack key={day.date} alignItems="center" spacing={2}>
+                        <Box sx={{ backgroundColor: index===3 ? "#000" : "#fff", width: "1.5rem", borderRadius: "100%", display: "flex", justifyContent: "center" }}>
+                            <Typography color={index===3 ? "#fff" : "#000"}>{day.day}</Typography>
+                        </Box>
+                        <Button onClick={handleClick(index)} color="secondary" sx={{ minWidth: "2.25rem", height: "2.25rem", borderRadius: "100%", padding: 0 }}>
+                            {flags[index] === true ?
+                                <Box sx={{ backgroundColor:  "accent.main", width: "2.25rem", height: "2.25rem", borderRadius: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                                    <DirectionsRunIcon sx={{ color: "#000" }} />
+                                </Box>
+                            :
+                                <Box sx={{ backgroundColor: "#DDDDDD", width: "2.25rem", height: "2.25rem", borderRadius: "100%" }} />
+                            }
+                        </Button>
+                    </Stack>
+                )} 
+            </Stack>
+        </Stack>
     )
 }
 
--- comfort-fe/src/utils/format.js
+++ comfort-fe/src/utils/format.js
@@ -1,11 +1,27 @@
-const dateToString = (source) => {
+function leftPad(value) {
+    if (value >= 10) {
+        return value;
+    }
+
+    return `0${value}`;
+}
+
+const dateToString = (source, delimeter) => {
     try {
         const year = source.getFullYear();
-        const month = source.getMonth() + 1;
-        const day = source.getDate();
-
-        const date = year + "년 " + month + "월 " + day + "일"
-
+        let month;
+        let day;
+        let date;
+        if (delimeter) {
+            month = leftPad(source.getMonth() + 1);
+            day = leftPad(source.getDate());
+            date = year + delimeter + month + delimeter + day;
+        }
+        else {
+            month = source.getMonth() + 1;
+            day = source.getDate();
+            date = year + "년 " + month + "월 " + day + "일"
+        }
         return date;
     }
     catch {
@@ -13,4 +29,31 @@ const dateToString = (source) => {
     }
 }
 
-export { dateToString }
+const getDayOfDate = (source) => {
+    const days = ["일", "월", "화", "수", "목", "금", "토"];
+
+    const day = days[source.getDay()];
+    return day;
+}
+
+const getDays = (source) => {
+    const today = source;
+    const currentDay = getDayOfDate(source);
+
+    const sevenDays = [];
+    const idx = [-3, -2, -1, 0, 1, 2, 3];
+    const days = ["일", "월", "화", "수", "목", "금", "토"];
+
+    let step;
+    let day;
+    let date = new Date();
+    for (step = 0; step < 7; step++) {
+        day = days[(days.indexOf(currentDay) + idx[step] + 7) % 7];
+        date.setDate(today.getDate() + idx[step]);
+        sevenDays.push({ "date": dateToString(date, "-"), "day": day });
+    }
+
+    return sevenDays;
+}
+
+export { dateToString, getDayOfDate, getDays }
